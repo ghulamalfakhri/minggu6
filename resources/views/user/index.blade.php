013d97531d5241d868cda9d844c96dac558953ca
ghulam <ghulamalfakhri@gmail.com>
2021-10-26 20:14:08 +0700

    uts

---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('USER DATA') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-succes" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <table class="table teble-responsive table-striped">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Email</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($user as $u)
+                            <tr>
+                                <td>{{ $u->name }}</td>
+                                <td>{{ $u->email }}</td>
+                                <td>
+                                    <form action="/users/{{$u->id}}" method="post">
+                                        <a href="/users/{{$u->id}}/edit" class="btn btn-warning">Edit</a>
+                                        <a href="/users/{{$u->id}}" class="btn btn-info">View</a>
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" name="delete" class="btn btn-danger">Delete</button>
+                                    </form>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                    <a href="/users/create" class="btn btn-primary">Add Data</a>
+                    <br><br>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,11 +14,14 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
-
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('students', StudentController::class); 
+Route::resource('students', StudentController::class);
+
+Route::resource('users', UserController::class); 
